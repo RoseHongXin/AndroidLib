d6673aec08518c1e3ee9db18744f32686544b81a
RoseHongXin <rose_hong@foxmail.com>
2017-06-28 09:59:49 +0800

    very important: smooth touch widgets update.

---
+++ src/views/smoothtouch/java/hx.view.smoothtouch/ISmooth.java
@@ -0,0 +1,16 @@
+package hx.view.smoothtouch;
+
+/**
+ * Created by Administrator on 2017/6/27 0027.
+ */
+
+public interface ISmooth {
+
+    int ANIMATION_DURATION = 1000;
+    float DEFAULT_DRAG_RATIO = 0.6f;
+
+    float MAXIMUM_VERTICAL_TOUCH_VELOCITY = 10000.0f;
+    float MAXIMUM_HORIZONTAL_TOUCH_VELOCITY = 3000.0f;
+
+    int MAXIMUM_VERTICAL_TOUCH_DELTA = 300;
+}
